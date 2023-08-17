d435bd3a3bf05e07b0438b69df2f8f53a5885001
Ben Lynch <bdlynch7@gmail.com>
2023-08-17 16:01:01 -0500

    started POST route, need to add another change so  pizzas array is sent with

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,24 +1,57 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
 function CustomerForm() {
 
-    const handleNameChange = (event) => {
+  const [infoToAdd, setInfoToAdd] = useState({ name: '', street: '', city: '', zip: '', type: '', total: '' })
 
+
+  const customerNext = (event) => {
+    axios.post('/order', infoToAdd)
+      .then((response) => {
+        // setInfoToAdd()
+      })
+      .catch((error) => {
+        console.error(`POST Error : ${error}`)
+      })
+      
+  }
+
+    const handleNameChange = (event) => {
+      setInfoToAdd({
+        ...infoToAdd,
+        name: event.target.value,
+      })
     }
 
     const handleStreetAddressChange = (event) => {
-        
+      setInfoToAdd({
+        ...infoToAdd,
+        street: event.target.value,
+      })
     }
 
     const handleCityChange = (event) => {
-        
+      setInfoToAdd({
+        ...infoToAdd,
+        city: event.target.value,
+      })
     }
 
     const handleZipChange = (event) => {
-        
+      setInfoToAdd({
+        ...infoToAdd,
+        zip: event.target.value,
+      })   
+    }
+    
+    const handleTypeChange = (event) => {
+      setInfoToAdd({
+        ...infoToAdd,
+        type: event.target.value
+      })
     }
 
-    const customerNext = (event) => {
-        
-    }
 
 
 
@@ -61,6 +94,8 @@ function CustomerForm() {
         />
 
         {/* TRUE/FALSE toggle? */}
+        <input type="radio" value="delivery">Delivery</input>
+        <input type="radio" value="pickup">Pickup</input>
 
         <button type="submit">NEXT</button>
       </form>
