b76bb94192fa84ab7b2543e14e54fc65711d17da
Ben Lynch <bdlynch7@gmail.com>
2023-08-18 10:37:18 -0500

    created table for checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,7 +1,21 @@
+import React, { useState } from "react"
+import axios from 'axios'
+
+
 function Checkout() {
 
+  axios.post('/api/order', newOrder)
+    .then((response) => {
+      
+    })
+
     return (<>
-    
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Cost</th>
+      </thead>
+    </table>
     
     </>)
 }
